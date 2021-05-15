1d427ef89c0c51ff179776cd823a7cc5bd74b405
Andrew Snodgrass <git@pointlander.info>
2021-05-15 09:21:31 -0600

    Added random float32 generator

--- main.go
+++ main.go
@@ -16,6 +16,21 @@ var LFSR = flag.Bool("lfsr", false, "find a lfsr")
 // LFSRMask is a LFSR mask with a maximum period
 const LFSRMask = 0x80000057
 
+// Rand is a random number generator
+type Rand uint32
+
+// Int63 returns a random float32 between 0 and 1
+func (r *Rand) Float32() float32 {
+	lfsr := *r
+	if (lfsr & 1 == 1) {
+		lfsr = (lfsr >> 1) ^ LFSRMask
+	} else {
+		lfsr = lfsr >> 1
+	}
+	*r = lfsr
+	return float32(lfsr) / ((1 << 32) - 1)
+}
+
 func main() {
 	flag.Parse()
 
@@ -43,9 +58,9 @@ func main() {
 		return
 	}
 
-	lfsr := uint32(1)
-	for i := 0; i < 10; i++ {
-		lfsr = (lfsr >> 1) ^ (-(lfsr & 1) & LFSRMask)
-		fmt.Println(lfsr)
+	rnd := Rand(0x55555555)
+	for i := 0; i < 32; i++ {
+		fmt.Println(rnd.Float32())
 	}	
 }
+
